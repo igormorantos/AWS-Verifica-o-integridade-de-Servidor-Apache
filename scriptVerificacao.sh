ca65e0826031696f6b98f60b17ed99f8c7a27f7d
igums <igormsantos1@icloud.com>
2024-02-07 17:39:58 -0300

    script que verificar se o servidor esta on

---
+++ scriptVerificacao.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Definir variáveis
+DATA=$(date +%d/%m/%Y)
+HORA=$(date +%H:%M:%S)
+SERVICO="httpd"
+
+# Verificar se o serviço está em execução
+if systemctl is-active "$SERVICO" > /dev/null; then
+  STATUS="active"
+else
+  STATUS="inactive"
+fi
+
+# Gerar mensagem
+MENSAGEM="O serviço $SERVICO está $STATUS"
+
+# Registrar mensagem no arquivo de log
+echo "$DATA $HORA - $SERVICO - $STATUS - $MENSAGEM" >> /mnt/nfs/igor/statusServer.txt
